2c0c1ee4d248401b62e08c65ab9c103d50ccf509
Benjamin Kay <benjamin@benkay.net>
2023-02-27 14:58:01 -0600

    Simplify DataProvider class

--- DataProvider.m
+++ DataProvider.m
@@ -2,33 +2,33 @@ classdef (Abstract) DataProvider < handle
     % Matlab class to provide neuroimaging data and associated metadata
     % such as motion/framewise displacement and non-imaging variables (e.g.
     % behavioral data). You must implement this class to feed your data
-    % into Shaman. See SimulatedDataProvider for a reference
+    % into Shaman. Specifically, you must implement nextData(),
+    % isMoreData(), and reset(). See SimulatedDataProvider for a reference
     % implementation.
     %
     % The DataProvider functions much like an iterator over study
     % participants. Participant's data is produced one-at-a-time, which
     % helps to economize memory use. Data can be obtained in an arbitrary
     % order using nextData(), which allows the underlying implementation to
-    % use asynchronous IO operations. Data can also be retrieved for a
-    % specific participant with dataAt().
+    % use asynchronous IO operations.
     methods (Abstract)
-        % Return the data for the participant identified by index.
-        % Index can be 1 through n_participants.
-        [data] = dataAt(this, index)
-        % Return the next participant's data along with the index of that
-        % participant. Each sequential call returns data from a different
-        % participant. Sequential calls can return data in any order (not
-        % necessarily 1, 2, 3...). Once data from every participant has
-        % been returned, subsequent calls to nextData() will return an
-        % empty data matrix [] and an index of 0, and isMoreData() will
-        % return false.
-        [data, index] = nextData(this)
+        % Return the next participant's data. Participants may be returned
+        % in any order, and the order may be different with different
+        % instantiations of the DataProvider or after a call to reset().
+        % For example, one instance may return 3 participants in the order
+        % 1, 2, 3. After calling reset it may return 1, 3, 2. Another
+        % instance may return 3, 2, 1. A duplicate subject may not be
+        % returned twice before a call to reset(), e.g. 1, 1, 2, 3 is not
+        % allowed. Once data from all participants have been returned,
+        % subsequent calls to nextData() should return an empty matrix []
+        % until reset() is called.
+        data = nextData(this)
         % Returns true if a call to nextData() will return more data, false
         % if there are no more participants to get data from.
         moreData = isMoreData(this)
-        % Resets the data counter so that isMoreData() will return true and
-        % the next call to nextData() will return data from the beginning
-        % again.
+        % Reset the state of the DataProvider. Equivalent to instantiating
+        % a new data provider. After calling reset(), subsequent calls to
+        % nextData() will return all of the participants again.
         reset(this)
     end
     methods
