ec4ceee621a4c42521e7de5a7e53d804b79ad7c3
Benjamin Kay <benjamin@benkay.net>
2023-11-24 12:33:19 -0600

    Fix bug in anynan

--- anynan.m
+++ anynan.m
@@ -1,8 +1,8 @@
 function tf = anynan(x)
     arguments 
-        x {isNumeric}
+        x {mustBeNumeric}
     end
     % Returns true if any element in matrix x is NaN, false otherwise.
     
-    tf = any(isnan(data.fmri(:)));
+    tf = any(isnan(x(:)));
 end
